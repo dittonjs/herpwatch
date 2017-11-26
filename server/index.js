2d03839cd5993ad1adca9dbf4f1eb5d2c84ed456
dittonjs <jditton.atomic@gmail.com>
2017-11-26 16:24:12 -0700

    only turn on one

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 const Gpio = require('onoff').Gpio;
-const pin2 = new Gpio(2, 'high');
-const pin3 = new Gpio(3, 'high');
-const pin4 = new Gpio(4, 'high');
-const pin17 = new Gpio(17, 'high');
-const pin27 = new Gpio(27, 'high');
-const pin22 = new Gpio(22, 'high');
-const pin10 = new Gpio(10, 'high');
-const pin9 = new Gpio(9, 'high');
+const pin2 = new Gpio(2, 'out');
+// const pin3 = new Gpio(3, 'out');
+// const pin4 = new Gpio(4, 'out');
+// const pin17 = new Gpio(17, 'out');
+// const pin27 = new Gpio(27, 'out');
+// const pin22 = new Gpio(22, 'out');
+// const pin10 = new Gpio(10, 'out');
+// const pin9 = new Gpio(9, 'out');
 
-setTimeout( () => pin2.writeSync(1) , 1000);
-setTimeout( () => pin3.writeSync(1) , 2000);
-setTimeout( () => pin4.writeSync(1) , 3000);
-setTimeout( () => pin17.writeSync(1) , 4000);
-setTimeout( () => pin27.writeSync(1) , 5000);
-setTimeout( () => pin22.writeSync(1) , 6000);
-setTimeout( () => pin10.writeSync(1) , 7000);
-setTimeout( () => pin9.writeSync(1) , 8000);
+// setTimeout( () => pin2.writeSync(1) , 1000);
+// setTimeout( () => pin3.writeSync(1) , 2000);
+// setTimeout( () => pin4.writeSync(1) , 3000);
+// setTimeout( () => pin17.writeSync(1) , 4000);
+// setTimeout( () => pin27.writeSync(1) , 5000);
+// setTimeout( () => pin22.writeSync(1) , 6000);
+// setTimeout( () => pin10.writeSync(1) , 7000);
+// setTimeout( () => pin9.writeSync(1) , 8000);
 
 // function test() {
 //   setTimeout( () => pin2.writeSync(1) , 1000);
@@ -28,22 +28,22 @@ setTimeout( () => pin9.writeSync(1) , 8000);
 //   setTimeout( () => pin9.writeSync(1) , 8000);
 // }
 // test();
-setTimeout(() => {
-  pin2.writeSync(0);
-  pin3.writeSync(0);
-  pin4.writeSync(0);
-  pin17.writeSync(0);
-  pin27.writeSync(0);
-  pin22.writeSync(0);
-  pin10.writeSync(0);
-  pin9.writeSync(0);
+// setTimeout(() => {
+//   pin2.writeSync(0);
+//   pin3.writeSync(0);
+//   pin4.writeSync(0);
+//   pin17.writeSync(0);
+//   pin27.writeSync(0);
+//   pin22.writeSync(0);
+//   pin10.writeSync(0);
+//   pin9.writeSync(0);
 
-  pin2.unexport();
-  pin3.unexport();
-  pin4.unexport();
-  pin17.unexport();
-  pin27.unexport();
-  pin22.unexport();
-  pin10.unexport();
-  pin9.unexport();
-}, 9000)
+//   pin2.unexport();
+//   pin3.unexport();
+//   pin4.unexport();
+//   pin17.unexport();
+//   pin27.unexport();
+//   pin22.unexport();
+//   pin10.unexport();
+//   pin9.unexport();
+// }, 9000)
