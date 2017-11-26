9867d8f7a0d7fbc95eea4371378fbebd2eae1023
dittonjs <jditton.atomic@gmail.com>
2017-11-26 15:51:45 -0700

    adds test

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import { setTimeout } from 'timers';
+
 const Gpio = require('onoff').Gpio;
 const pin2 = new Gpio(2, 'out');
 const pin3 = new Gpio(3, 'out');
@@ -7,36 +9,19 @@ const pin27 = new Gpio(27, 'out');
 const pin22 = new Gpio(22, 'out');
 const pin10 = new Gpio(10, 'out');
 const pin9 = new Gpio(9, 'out');
-let trigger = true;
 
-function toggleOne() {
-  if(trigger) {
-    pin2.writeSync(1);
-    pin3.writeSync(1);
-    pin4.writeSync(1);
-    pin17.writeSync(1);
-    pin27.writeSync(1);
-    pin22.writeSync(1);
-    pin10.writeSync(1);
-    pin9.writeSync(1);
-    trigger = false;
-  } else {
-    pin2.writeSync(0);
-    pin3.writeSync(0);
-    pin4.writeSync(0);
-    pin17.writeSync(0);
-    pin27.writeSync(0);
-    pin22.writeSync(0);
-    pin10.writeSync(0);
-    pin9.writeSync(0);
-    trigger = true;
-  }
+function test() {
+  setTimeout( () => pin2.writeSync(1) , 1000);
+  setTimeout( () => pin3.writeSync(1) , 2000);
+  setTimeout( () => pin4.writeSync(1) , 3000);
+  setTimeout( () => pin17.writeSync(1) , 4000);
+  setTimeout( () => pin27.writeSync(1) , 5000);
+  setTimeout( () => pin22.writeSync(1) , 6000);
+  setTimeout( () => pin10.writeSync(1) , 7000);
+  setTimeout( () => pin9.writeSync(1) , 8000);
 }
-
-const interval = setInterval(toggleOne, '1000');
-
+test();
 setTimeout(() => {
-  clearInterval(interval);
   pin2.writeSync(0);
   pin3.writeSync(0);
   pin4.writeSync(0);
@@ -54,4 +39,4 @@ setTimeout(() => {
   pin22.unexport();
   pin10.unexport();
   pin9.unexport();
-}, 10000)
+}, 9000)
