e9b3bbb27908c7313063e5c36d49486782bc09a4
dittonjs <jditton.atomic@gmail.com>
2017-12-01 13:46:11 -0700

    type of

--- server/test_temps.js
+++ server/test_temps.js
@@ -3,7 +3,8 @@ ds18b20.sensors(function(err, ids) {
   console.log(ids);
   setInterval(() => {
     ds18b20.temperature(ids[0], (err, value) => {
-      console.log(`temp is ${value}C`);
+
+      console.log(`temp is ${value}C`, console.log(typeof value));
     })
   }, 1000)
 });
