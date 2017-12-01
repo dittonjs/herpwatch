7d3c2914509c9007383d40ea23963cbddbaeb2c7
dittonjs <jditton.atomic@gmail.com>
2017-12-01 13:33:40 -0700

    looping script;

--- server/test_temps.js
+++ server/test_temps.js
@@ -1,4 +1,9 @@
 var ds18b20 = require('ds18b20');
 ds18b20.sensors(function(err, ids) {
   console.log(ids);
+  setInterval(() => {
+    ds18b20.temperature(ids[0], (err, value) => {
+      console.log(`temp is ${value}C`);
+    })
+  }, 1000)
 });
