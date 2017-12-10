2afabb454d1798ddc498226c3ffcf5ea150906bd
dittonjs <jditton.atomic@gmail.com>
2017-12-09 22:55:55 -0700

    test for errors

--- pi_server/test_temps.js
+++ pi_server/test_temps.js
@@ -3,7 +3,7 @@ ds18b20.sensors(function(err, ids) {
   console.log(ids);
   setInterval(() => {
     ds18b20.temperature(ids[0], (err, value) => {
-
+      err && console.log(err);
       console.log(`temp is ${value}C`);
     })
   }, 1000)
